2e0aac28dfce01c8a7fa24077b80dcc85aeee88c
Guangqi <363305719@qq.com>
2018-10-26 22:57:25 -0400

    read cmd from argv

--- hw2/command.c
+++ hw2/command.c
@@ -1,62 +1,87 @@
+#include <sys/wait.h>
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 int command(const char *string, char *outbuf, int outlen, char *errbuf, int errlen);
 int main(int argc, char *argv[]){
-  char out[BUFSIZ], err[BUFSIZ];
-  if (command("ls -la", out, BUFSIZ, err, BUFSIZ) == -1) {
-    perror("command");
-    exit(EXIT_FAILURE);
-  }
-  printf("stdout:\n%s\n", out);
-  printf("stderr:\n%s\n", err);
-  return EXIT_SUCCESS;
+  
+    int i;
+    char cmd[BUFSIZ];
+    for (i = 1; i < argc; i++) {		/* read command from terminal */
+	strcat(cmd, argv[i]);
+	strcat(cmd," ");
+    }
+  
+    char out[BUFSIZ], err[BUFSIZ];
+    if (command(cmd, out, BUFSIZ, err, BUFSIZ) == -1) {
+	perror("command");
+	exit(EXIT_FAILURE);
+    }
+    printf("stdout:\n%s\n", out);
+    printf("stderr:\n%s\n", err);
+    return EXIT_SUCCESS;
 }
 
 int command(const char *string, char *outbuf, int outlen, char *errbuf, int errlen){
-  int outfd[2];
-  int errfd[2];
-  int pid,read_num,lenth;
-  if(pipe(errfd) < 0){
-    perror("create errfd error");
-    return -1;
-  }
-  if(pipe(outfd) < 0){
-    perror("create errfd error");
-    return -1;
-  }
-  if((pid = fork()) <0){
-    perror("fork error");
-    exit(127);
-  }
-  if(pid > 0){
-    close(outfd[0]);
-    if(dup2(outfd[1],STDOUT_FILENO) != STDOUT_FILENO){
-      perror("dup2 error");
-      return -1;
+    int outfd[2];
+    int errfd[2];
+    int pid,lenth;
+    if(string == NULL || strlen(string) == 0){
+	perror("null command");
+	return -1;
     }
-    close(errfd[0]);
-    if (dup2(errfd[1], STDERR_FILENO) != STDERR_FILENO) {
-      perror("dup2 error");
-      return -1;
+    if(pipe(errfd) < 0){
+	perror("create errfd error");
+	return -1;
     }
-    execlp("/bin/sh","sh","-c",string, NULL);
-    close(outfd[1]);
-    close(errfd[1]);
-    if (waitpid(pid, NULL, 0) < 0) {
-      perror("waitpid error");
-      return -1;
+    if(pipe(outfd) < 0){
+	perror("create outfd error");
+	return -1;
     }
-  }
-  if(pid == 0) {
-    close(outfd[1]);
-    close(errfd[1]);
-//    if (dup2(outfd[0], STDIN_FILENO) != STDIN_FILENO) {}
+    if((pid = fork()) <0){
+	perror("fork error");
+	exit(127);
+    }
+    if(pid > 0){ /* father write ls to pipe */
+	close(outfd[0]);
+	/* make out to outfd1 */
+	if(dup2(outfd[1],STDOUT_FILENO) != STDOUT_FILENO){
+	    perror("dup2 error");
+	    return -1;
+	}
+	close(errfd[0]);
+	/*error to errfd */
+	if (dup2(errfd[1], STDERR_FILENO) != STDERR_FILENO) {
+	    perror("dup2 error");
+	    return -1;
+	}
+	execlp("/bin/sh","sh","-c",string, NULL);
+	close(outfd[1]);
+	close(errfd[1]);
+	if (waitpid(pid, NULL, 0) < 0) {
+	    perror("waitpid error");
+	    return -1;
+	}
+    }
+    if(pid == 0) { /* chilid read from the pipe */
+	close(outfd[1]);
+	close(errfd[1]);
     
-    read(outfd[0], outbuf, outlen);
-    read(errfd[0], errbuf, errlen);
-  }
+	if((lenth = read(outfd[0], outbuf, outlen)) == -1){
+	    perror("read pipe error");
+	    return -1;
+	}
+	if((lenth = read(errfd[0], errbuf, errlen)) == -1){
+	    perror("read pipe error");
+	    return -1;
+	}
+	outbuf[BUFSIZ - 1] = '\0'; /* reduce mojibake */
+	errbuf[BUFSIZ - 1] = '\0';
+	close(outfd[0]);
+	close(errfd[0]);
+    }
   
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
